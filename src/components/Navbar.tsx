70feda2311e1a9cc7bab28b9a7b894517379f237
aimennsou <150556045+aimennsou@users.noreply.github.com>
2024-04-03 09:50:46 +0200

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
 import { Button } from "./ui/button";
-
 
 
 
@@ -21,14 +19,14 @@ const Navbar = () => {
     <div className="relative mx-auto max-w-7xl px-6 lg:px-8 flex items-center justify-between z-1000">
           <div className="p-4 ">
               <Link href="/">
-                  <Image src="/logo.png" alt="Mon emploi" width={140} height={80} />
+                  <Image src="/logo.png" alt="lebondiplome" width={140} height={80} />
               </Link>
           </div>
      
               <ul className="flex  items-center gap-6">
                  
                   <p>
-                  <Link href="https://github.com/aimenonly">
+                  <Link href="https://github.com/aimenonly/lebondiplome">
                       <Button className="hover:border-green-700" variant="outline"> Voir le code source </Button>
                       </Link>
                   </p>
